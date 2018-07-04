cbfcbcb56753e9793ca74aefc8bbbd9971d0f30f
andyt530 <andyt530@outlook.com>
2018-07-04 17:53:07 +0100

    more hpa

--- hpa/stress.sh
+++ hpa/stress.sh
@@ -1,5 +1,6 @@
 helm install stable/metrics-server
 helm install stable/metrics-server --namespace cpu-example
+# don't install helm
 kubectl get apiservices | grep metrics
 kubectl create namespace cpu-example
 kubectl create -f stress.yaml --namespace=cpu-example
